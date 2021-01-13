a1e35df474a1193a4899bcd5de55f6fd0b53a9d1
Laura Toro <lttoro20@gmail.com>
2021-01-12 17:54:21 -0800

    Initial Commit

---
+++ js/mywebsite.js
@@ -0,0 +1,31 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Closes the sidebar menu
+  $(".menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+    $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
+    $(this).toggleClass("active");
+  });
+
+
+  //fade header when scrolling up
+  
+  $(window).scroll(function(){
+  	$(".masthead").css("opacity", 1 -
+  $(window).scrollTop() / 500);
+  });
+
+
+
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('#sidebar-wrapper .js-scroll-trigger').click(function() {
+    $("#sidebar-wrapper").removeClass("active");
+    $(".menu-toggle").removeClass("active");
+    $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
+  });
+
+
+})(jQuery); // End of use strict
